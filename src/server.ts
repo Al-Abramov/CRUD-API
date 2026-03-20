e9418e50125a9cb7243e4b7719531bd44edebdf1
Абрамов Алексей <x3m.f@mail.ru>
2026-03-20 19:40:40 +0400

    feat add tests

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
-import Fastify from 'fastify';
 import dotenv from 'dotenv';
-import { productRoutes } from './routes/products.js';
+import { buildApp } from './app.js';
 
 dotenv.config();
 
-const app = Fastify();
+const app = buildApp();
 
 const PORT = Number(process.env.PORT) || 4000;
-
-app.register(productRoutes);
-
-app.setErrorHandler((error, request, reply) => {
-  app.log.error(error);
-
-  return reply.status(500).send({
-    message: 'Internal server error',
-  });
-});
-
-app.setNotFoundHandler((request, reply) => {
-  return reply.status(404).send({
-    message: 'Route not found',
-  });
-});
 
 const start = async () => {
   try {
